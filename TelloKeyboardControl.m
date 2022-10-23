6fca160c3e644aa1593de856a8eeea4d7637ddde
jdanielhoyos <58446071+jdanielhoyos@users.noreply.github.com>
2022-10-23 00:03:42 -0400

    Functions

---
+++ TelloKeyboardControl.m
@@ -0,0 +1,13 @@
+clear all
+clc 
+close all
+global myDrone
+myDrone = ryze();
+myCam = camera(myDrone);
+takeoff(myDrone);
+h_fig = figure;
+hImage = image( zeros(720, 960, 1) ); 
+preview(myCam, hImage); 
+
+set(h_fig,'KeyPressFcn',@KeyDetectTello);
+
